d28903ea08a24cd0128921f8deb9f17cd29a3707
Himansh Mudigonda <himudigonda@gmail.com>
2024-09-04 21:49:13 -0700

    Update awards.js and honors.js files to remove end date if not req

--- data/awards.js
+++ data/awards.js
@@ -1,10 +1,11 @@
+const awd_items = [
+  {
+    jobTitle: 'Gold Medalist: Research Day',
+    company: 'SRM University',
+    startDate: '2023-04',
+    // endDate: '2023-04-16',
+    location: 'SRM University, AP, India',
+  },
+]
 
-const awd_items = [{
-  jobTitle: 'Gold Medalist: Research Day',
-  company: 'SRM University',
-  startDate: '2023-04',
-  endDate: '2023-04-16',
-  location: 'SRM University, AP, India'
-},]
-
-export default awd_items;
+export default awd_items
--- pages/honors.js
+++ pages/honors.js
@@ -8,135 +8,129 @@ import sc_items from '../data/scholarships'
 import awd_items from '../data/awards'
 
 export async function getStaticProps() {
-    return {
-        props: {
-            title: 'Honors// Himansh Mudigonda',
-            tagline: 'Respect. Power. Banana.',
-            image: '/static/images/articles-bw.jpg',
-            primaryColor: 'white', secondaryColor: 'blue'
-        },
-    }
+  return {
+    props: {
+      title: 'Honors// Himansh Mudigonda',
+      tagline: 'Respect. Power. Banana.',
+      image: '/static/images/articles-bw.jpg',
+      primaryColor: 'white',
+      secondaryColor: 'blue',
+    },
+  }
 }
 
 function Honors(props) {
-    const renderSC = () => {
-        return sc_items.map((item, index) => {
-            return (
-                <div style={{ marginBottom: 40 }} key={index}>
-                    <h3>{item.jobTitle}</h3>
-                    <p style={{ margin: 0 }}>
-                        <a href={item.companyUrl} target="_blank">
-                            {item.company}
-                        </a>
-                        <span> • {item.location}</span>
-                    </p>
-                    <p style={{ margin: 0 }}>
-                        <span>{format(parseISO(item.startDate), 'LLL yyyy')}</span>
-                        <span> – </span>
-                        <span>
-                            {item.endDate
-                                ? format(parseISO(item.endDate), 'LLL yyyy')
-                                : 'Present'}
-                        </span>
-                        {/* <span> • </span> */}
-                        {/* <span>{getDuration(item.startDate, item.endDate)}</span> */}
-                    </p>
-                </div>
-            )
-        })
+  const renderSC = () => {
+    return sc_items.map((item, index) => {
+      return (
+        <div style={{ marginBottom: 40 }} key={index}>
+          <h3>{item.jobTitle}</h3>
+          <p style={{ margin: 0 }}>
+            <a href={item.companyUrl} target="_blank">
+              {item.company}
+            </a>
+            <span> • {item.location}</span>
+          </p>
+          <p style={{ margin: 0 }}>
+            <span>{format(parseISO(item.startDate), 'LLL yyyy')}</span>
+            <span> – </span>
+            <span>
+              {item.endDate
+                ? format(parseISO(item.endDate), 'LLL yyyy')
+                : 'Present'}
+            </span>
+            {/* <span> • </span> */}
+            {/* <span>{getDuration(item.startDate, item.endDate)}</span> */}
+          </p>
+        </div>
+      )
+    })
+  }
+
+  const renderAwd = () => {
+    return awd_items.map((item, index) => {
+      return (
+        <div style={{ marginBottom: 40 }} key={index}>
+          <h3>{item.jobTitle}</h3>
+          <p style={{ margin: 0 }}>
+            <a href={item.companyUrl} target="_blank">
+              {item.company}
+            </a>
+            <span> • {item.location}</span>
+          </p>
+          <p style={{ margin: 0 }}>
+            <span>{format(parseISO(item.startDate), 'LLL yyyy')}</span>
+            <span>
+              {item.endDate ? format(parseISO(item.endDate), 'LLL yyyy') : ''}
+            </span>
+            {/* <span> • </span> */}
+            {/* <span>{getDuration(item.startDate, item.endDate)}</span> */}
+          </p>
+        </div>
+      )
+    })
+  }
+
+  const getDuration = (startDate, endDate) => {
+    const durationObj = intervalToDuration({
+      start: parseISO(startDate),
+      end: endDate ? parseISO(endDate) : new Date(),
+    })
+
+    let durationStr = ''
+
+    if (durationObj.years > 1) {
+      durationStr = `${durationObj.years} yrs `
+    } else if (durationObj.years === 1) {
+      durationStr = `${durationObj.years} yr `
     }
 
-    const renderAwd = () => {
-        return awd_items.map((item, index) => {
-            return (
-                <div style={{ marginBottom: 40 }} key={index}>
-                    <h3>{item.jobTitle}</h3>
-                    <p style={{ margin: 0 }}>
-                        <a href={item.companyUrl} target="_blank">
-                            {item.company}
-                        </a>
-                        <span> • {item.location}</span>
-                    </p>
-                    <p style={{ margin: 0 }}>
-                        <span>{format(parseISO(item.startDate), 'LLL yyyy')}</span>
-                        <span> – </span>
-                        <span>
-                            {item.endDate
-                                ? format(parseISO(item.endDate), 'LLL yyyy')
-                                : 'Present'}
-                        </span>
-                        {/* <span> • </span> */}
-                        {/* <span>{getDuration(item.startDate, item.endDate)}</span> */}
-                    </p>
-                </div>
-            )
-        })
-    }
+    durationStr += `${durationObj.months} mos`
 
-    const getDuration = (startDate, endDate) => {
-        const durationObj = intervalToDuration({
-            start: parseISO(startDate),
-            end: endDate ? parseISO(endDate) : new Date(),
-        })
+    return durationStr
+  }
 
-        let durationStr = ''
+  const { title, image } = props
+  const description = `Here are a few of my honors, awards, scholarships and certifications</strong>.`
 
-        if (durationObj.years > 1) {
-            durationStr = `${durationObj.years} yrs `
-        } else if (durationObj.years === 1) {
-            durationStr = `${durationObj.years} yr `
-        }
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta content={title} property="og:title" />
+        <meta content={stripHtml(description)} name="description" />
+        <meta content={stripHtml(description)} property="og:description" />
+        <meta content="https://himudigonda.me/honors" property="og:url" />
+      </Head>
 
-        durationStr += `${durationObj.months} mos`
-
-        return durationStr
-    }
-
-
-
-    const { title, image } = props
-    const description = `Here are a few of my honors, awards, scholarships and certifications</strong>.`
-
-    return (
-        <>
-            <Head>
-                <title>{title}</title>
-                <meta content={title} property="og:title" />
-                <meta content={stripHtml(description)} name="description" />
-                <meta content={stripHtml(description)} property="og:description" />
-                <meta content="https://himudigonda.me/honors" property="og:url" />
-            </Head>
-
-            <h2>Scholarships</h2>
-            {renderSC()}
-            <h2>Awards</h2>
-            {renderAwd()}
-
-
-        </>
-    )
+      <h2>Scholarships</h2>
+      {renderSC()}
+      <h2>Awards</h2>
+      {renderAwd()}
+    </>
+  )
 }
 
 const Container = styled('div', {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-    '@bp2': { flexDirection: 'row' },
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+  '@bp2': { flexDirection: 'row' },
 })
 
 const Paragraph = styled('p', {
-    '@bp2': { margin: '15px 0' },
+  '@bp2': { margin: '15px 0' },
 })
 
 const ButtonsContainer = styled('p', {
-    display: 'flex',
-    alignItems: 'center',
+  display: 'flex',
+  alignItems: 'center',
 })
 
 const Section = styled('div', {
-    marginTop: '0px',
-    width: 'auto',
-    '@bp2': { width: '48%' },
+  marginTop: '0px',
+  width: 'auto',
+  '@bp2': { width: '48%' },
 })
 
 Honors.Layout = Base

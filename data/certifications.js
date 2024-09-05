3755068703af6b7062183ae1d68a364b8924c9e3
Himansh Mudigonda <himudigonda@gmail.com>
2024-09-04 18:01:05 -0700

    updated certifications end date format

--- data/certifications.js
+++ data/certifications.js
@@ -1,61 +1,62 @@
-
 const cert_items = [
   {
     jobTitle: 'Google AI Essentials',
     company: 'Google',
     startDate: '2024-08-01',
-    endDate: '2024-08-01',
-    location: 'Coursera'
+    // endDate: '2024-08-01',
+    location: 'Coursera',
   },
   {
-    jobTitle: 'Improving Deep Neural Networks: Hyperparameter Tuning, Regularization and Optimization',
+    jobTitle:
+      'Improving Deep Neural Networks: Hyperparameter Tuning, Regularization and Optimization',
     company: 'DeepLearning.AI',
     startDate: '2024-08-01',
-    endDate: '2024-08-01',
-    location: 'Coursera'
+    // endDate: '2024-08-01',
+    location: 'Coursera',
   },
   {
     jobTitle: 'Neural Networks and Deep Learning',
     company: 'DeepLearning.AI',
     startDate: '2024-08-01',
-    endDate: '2024-08-01',
-    location: 'Coursera'
+    // endDate: '2024-08-01',
+    location: 'Coursera',
   },
   {
     jobTitle: 'Supervised Machine Learning: Regression and Classification ',
     company: 'Stanford University',
     startDate: '2024-08-01',
-    endDate: '2024-08-01',
-    location: 'Coursera'
+    // endDate: '2024-08-01',
+    location: 'Coursera',
   },
   {
     jobTitle: 'Data or Specimens Only Research',
     company: 'Massachusetts Institute of Technology Affiliates',
     startDate: '2023-09-01',
     endDate: '2026-09-01',
-    location: 'CITI Program'
+    location: 'CITI Program',
   },
   {
     jobTitle: 'Getting Started with Enterprise - grade AI',
     company: 'IBM',
     startDate: '2021-07-01',
-    endDate: '2021-07-01',
-    location: 'IBM'
+    // endDate: '2021-07-01',
+    location: 'IBM',
   },
   {
-    jobTitle: 'Getting Started with Cloud for the EnterpriseGetting Started with Cloud for the Enterprise',
+    jobTitle:
+      'Getting Started with Cloud for the EnterpriseGetting Started with Cloud for the Enterprise',
     company: 'IBM',
     startDate: '2021-07-01',
-    endDate: '2021-07-01',
-    location: 'IBM'
+    // endDate: '2021-07-01',
+    location: 'IBM',
   },
   {
     jobTitle: 'Getting Started with Enterprise Data Science',
     company: 'IBM',
     startDate: '2021-07-01',
-    endDate: '2021-07-01',
-    location: 'IBM'
+    // endDate: '2021-07-01',
+    location: 'IBM',
   },
-];
+]
 
-export default cert_items;
+export default cert_items
--- pages/certifications.js
+++ pages/certifications.js
@@ -7,103 +7,103 @@ import stripHtml from '../lib/strip-html'
 import cert_items from '../data/certifications'
 
 export async function getStaticProps() {
-    return {
-        props: {
-            title: 'Certifications // Himansh Mudigonda',
-            tagline: 'Credibility. Practise. Knowledge.',
-            image: '/static/images/articles-bw.jpg',
-            primaryColor: 'white', secondaryColor: 'orange'
-        },
-    }
+  return {
+    props: {
+      title: 'Certifications // Himansh Mudigonda',
+      tagline: 'Credibility. Practise. Knowledge.',
+      image: '/static/images/articles-bw.jpg',
+      primaryColor: 'white',
+      secondaryColor: 'orange',
+    },
+  }
 }
 
 function Honors(props) {
+  const renderCert = () => {
+    return cert_items.map((item, index) => {
+      return (
+        <div style={{ marginBottom: 40 }} key={index}>
+          <h3>{item.jobTitle}</h3>
+          <p style={{ margin: 0 }}>
+            <a href={item.companyUrl} target="_blank">
+              {item.company}
+            </a>
+            <span> • {item.location}</span>
+          </p>
+          <p style={{ margin: 0 }}>
+            <span>{format(parseISO(item.startDate), 'LLL yyyy')}</span>
+            <span> – </span>
+            <span>
+              {item.endDate
+                ? format(parseISO(item.endDate), 'LLL yyyy')
+                : 'Forever'}
+            </span>
+            {/* <span> • </span> */}
+            {/* <span>{getDuration(item.startDate, item.endDate)}</span> */}
+          </p>
+        </div>
+      )
+    })
+  }
 
-    const renderCert = () => {
-        return cert_items.map((item, index) => {
-            return (
-                <div style={{ marginBottom: 40 }} key={index}>
-                    <h3>{item.jobTitle}</h3>
-                    <p style={{ margin: 0 }}>
-                        <a href={item.companyUrl} target="_blank">
-                            {item.company}
-                        </a>
-                        <span> • {item.location}</span>
-                    </p>
-                    <p style={{ margin: 0 }}>
-                        <span>{format(parseISO(item.startDate), 'LLL yyyy')}</span>
-                        <span> – </span>
-                        <span>
-                            {item.endDate
-                                ? format(parseISO(item.endDate), 'LLL yyyy')
-                                : 'Present'}
-                        </span>
-                        {/* <span> • </span> */}
-                        {/* <span>{getDuration(item.startDate, item.endDate)}</span> */}
-                    </p>
-                </div>
-            )
-        })
+  const getDuration = (startDate, endDate) => {
+    const durationObj = intervalToDuration({
+      start: parseISO(startDate),
+      end: endDate ? parseISO(endDate) : new Date(),
+    })
+
+    let durationStr = ''
+
+    if (durationObj.years > 1) {
+      durationStr = `${durationObj.years} yrs `
+    } else if (durationObj.years === 1) {
+      durationStr = `${durationObj.years} yr `
     }
 
-    const getDuration = (startDate, endDate) => {
-        const durationObj = intervalToDuration({
-            start: parseISO(startDate),
-            end: endDate ? parseISO(endDate) : new Date(),
-        })
+    durationStr += `${durationObj.months} mos`
 
-        let durationStr = ''
+    return durationStr
+  }
 
-        if (durationObj.years > 1) {
-            durationStr = `${durationObj.years} yrs `
-        } else if (durationObj.years === 1) {
-            durationStr = `${durationObj.years} yr `
-        }
+  const { title, image } = props
 
-        durationStr += `${durationObj.months} mos`
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta content={title} property="og:title" />
+        <meta
+          content="https://himudigonda.me/certifications"
+          property="og:url"
+        />
+      </Head>
 
-        return durationStr
-    }
-
-
-
-    const { title, image } = props
-
-    return (
-        <>
-            <Head>
-                <title>{title}</title>
-                <meta content={title} property="og:title" />
-                <meta content="https://himudigonda.me/certifications" property="og:url" />
-            </Head>
-
-            <h2>Certifications</h2>
-            {renderCert()}
-
-        </>
-    )
+      <h2>Certifications</h2>
+      {renderCert()}
+    </>
+  )
 }
 
 const Container = styled('div', {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-    '@bp2': { flexDirection: 'row' },
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+  '@bp2': { flexDirection: 'row' },
 })
 
 const Paragraph = styled('p', {
-    '@bp2': { margin: '15px 0' },
+  '@bp2': { margin: '15px 0' },
 })
 
 const ButtonsContainer = styled('p', {
-    display: 'flex',
-    alignItems: 'center',
+  display: 'flex',
+  alignItems: 'center',
 })
 
 const Section = styled('div', {
-    marginTop: '0px',
-    width: 'auto',
-    '@bp2': { width: '48%' },
+  marginTop: '0px',
+  width: 'auto',
+  '@bp2': { width: '48%' },
 })
 
 Honors.Layout = Base

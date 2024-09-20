64f12db28e065c1a6c9c7ce802ddd9e183736dfa
Himansh Mudigonda <himudigonda@gmail.com>
2024-09-19 17:30:36 -0700

    updated exp

--- data/about.js
+++ data/about.js
@@ -1,54 +1,54 @@
-
 const items = [
+  {
+    jobTitle: 'Research Assistant, Reinforcement Learning',
+    company: 'Professor Shaofeng Zou',
+    startDate: '2024-09-19',
+    endDate: '',
+    location: 'Arizona State University, Tempe, USA',
+  },
   {
     jobTitle: 'Staff Machine Learning Intern, Health',
     company: 'Endimension LLC',
     startDate: '2024-04-01',
     endDate: '',
-    location: 'Remote - Mumbai, India'
+    location: 'Remote - Mumbai, India',
   },
   {
     jobTitle: 'Teaching Assistant & Grader: TRN2024: Bio-medical Imaging',
     company: 'College of Health Solutions',
     startDate: '2024-05-01',
     endDate: '2024-08-02',
-    location: 'Arizona State University, Tempe, USA'
+    location: 'Arizona State University, Tempe, USA',
   },
   {
     jobTitle: 'Multimodal AI Researcher, Medical Image Analysis',
     company: 'Professor Jianming Liang',
-    startDate: '2023-12-20',
-    endDate: '2024-08-19',
-    location: 'Arizona State University, Tempe, USA'
-  },
-  {
-    jobTitle: 'Deep Learning Researcher, Medical Image Analysis',
-    company: 'Professor Jianming Liang',
     startDate: '2023-09-01',
-    endDate: '2023-12-19',
-    location: 'Arizona State University, Tempe, USA'
+    endDate: '2024-09-01',
+    location: 'Arizona State University, Tempe, USA',
   },
   {
     jobTitle: 'Machine Learning, Software Development',
     company: 'SRM Advanced Electronics Laboratory',
     startDate: '2022-10-01',
     endDate: '2023-07-31',
-    location: 'SRM University, Amaravathi, India'
+    location: 'SRM University, Amaravathi, India',
   },
   {
-    jobTitle: 'Teaching Assistant & Grader: CSE-201: Design Analysis of Algorithms',
+    jobTitle:
+      'Teaching Assistant & Grader: CSE-201: Design Analysis of Algorithms',
     company: 'School of Engineering and Applied Sciences',
     startDate: '2021-08-01',
     endDate: '2023-07-31',
-    location: 'SRM University, Amaravathi, India'
+    location: 'SRM University, Amaravathi, India',
   },
   {
     jobTitle: 'Cognitive AI Researcher, Machine Learning',
     company: 'NeXtTech LLC',
     startDate: '2019-11-01',
     endDate: '2022-10-31',
-    location: 'Chennai, India'
-  }
-];
+    location: 'Chennai, India',
+  },
+]
 
-export default items;
+export default items

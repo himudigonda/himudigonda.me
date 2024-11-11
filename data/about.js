f0d8b650366e7385f8b244e943c6c1f01705f654
Himansh Mudigonda <himudigonda@gmail.com>
2024-11-10 22:49:54 -0700

    feat: update job titles for clarity and consistency in about data

--- data/about.js
+++ data/about.js
@@ -1,6 +1,6 @@
 const items = [
   {
-    jobTitle: 'Staff Machine Learning Intern, Health',
+    jobTitle: 'Machine Learning Intern',
     company: 'Endimension LLC',
     startDate: '2024-04-01',
     endDate: '2024-08-01',
@@ -14,7 +14,7 @@ const items = [
     location: 'Arizona State University, Tempe, USA',
   },
   {
-    jobTitle: 'Multimodal AI Researcher, Medical Image Analysis',
+    jobTitle: 'Computer Vision Researcher',
     company: 'Professor Jianming Liang',
     startDate: '2023-09-01',
     endDate: '2024-09-01',
@@ -36,7 +36,7 @@ const items = [
     location: 'SRM University, Amaravathi, India',
   },
   {
-    jobTitle: 'Cognitive AI Researcher, Machine Learning',
+    jobTitle: 'Artificial Intelligence Researcher',
     company: 'NeXtTech LLC',
     startDate: '2019-11-01',
     endDate: '2022-10-31',
